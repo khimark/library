761f7ede1f822f168aa99af4f0e2f765b473321b
khimark <90699151+khimark@users.noreply.github.com>
2021-10-26 16:15:13 +0300

    Add files via upload

---
+++ project.c
@@ -0,0 +1,35 @@
+/****************************
+   Project; Library system
+   Author; Mark Kim
+   Date; October 2021
+   Language; C99
+   Compiler; GCC
+   License; MIT
+
+***************************/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+int main()
+{
+    printf("\tCounty Library Systems\n");
+    printf("login");
+    printf("Welcome Mr. Kim\n");
+    printf("What would you like to\n");
+    printf("1, Add User\n");
+    printf("2, Add Book\n");
+    printf("2, Borrow Book\n");
+    printf("1, Delete User\n");
+    printf("2, Delete Book\n");
+    printf("2, Return Book\n");
+    printf("login out");
+    printf("Action");
+
+
+
+    return 0;
+
+}
